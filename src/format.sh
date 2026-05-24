fd95284fbbdb8901f163d17d172c78657a056954
Hunter <hunter.dsp@gmail.com>
2026-05-23 22:26:29 -0400

    chore: release v0.1.5

--- src/format.sh
+++ src/format.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # ############################################################################
 # LIBRARY: format.sh                                                         #
-# PACKAGE: just-bashit version 0.1.4                                         #
+# PACKAGE: just-bashit version 0.1.5                                         #
 # ############################################################################
 
 # Enforce sourcing of the script by taking advantage of the fact that return
