29234d57dcb3b9485781e99ab9e152eb149c8829
hunterdsp <hunter.dsp@gmail.com>
2026-08-03 08:49:59 -0400

    chore: release v0.3.0

--- src/just_bashit/bashrc-template.sh
+++ src/just_bashit/bashrc-template.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # ############################################################################
 # TEMPLATE: bashrc-template.sh                                               #
-# PACKAGE: just-bashit version 0.2.0                                         #
+# PACKAGE: just-bashit version 0.3.0                                         #
 # ############################################################################
 # Opinionated, cross-distro interactive bash configuration.                  #
 #                                                                            #
